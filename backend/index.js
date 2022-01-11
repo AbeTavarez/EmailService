4ee114ba4ef17b2deef3f20c93fa17d5c8261665
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-01-10 19:17:05 -0500

    server init setup

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({msg: 'Hello'})
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
